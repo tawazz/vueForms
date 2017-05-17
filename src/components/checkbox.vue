87a3fcb65f0dd61880d297b76ecac14acdc5922d
tawanda <tawanda.nyakudjga@gmail.com>
2017-05-17 16:59:39 +0800

    checkbox component

---
+++ src/components/checkbox.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+    <div>
+        <div class="form-group">
+            <div class="checkbox">
+                <label>
+                    <input :name="name" type="checkbox" data-parsley-required :data-conditions="options" @change="handleCheckBoxChange" />
+                    {{ label }}
+                </label>
+            </div>
+            <p class="help-block">{{ help_text }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['name','label','help_text','conditions',"handleCheckBoxChange"],
+    computed:{
+        isChecked:function () {
+            //TODO return value from database
+            return false;
+        },
+        options:function () {
+            return JSON.stringify(this.conditions);
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
