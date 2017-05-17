4a4ac7b6604f2ff82f0de7d373fabe1888dbb883
tawanda <tawanda.nyakudjga@gmail.com>
2017-05-17 17:00:47 +0800

    radio button component

---
+++ src/components/radio.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+    <div>
+        <div class="radio">
+            <label>
+                <input :name="name" type="radio" :value="value" @change="handleRadioChange"  :required="isRequired" :data-conditions="options" :checked="isChecked"/>{{ label}}
+            </label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"radiobuttons",
+    props:["value","label","name","isRequired","handleRadioChange","conditions"],
+    computed:{
+        isChecked:function () {
+            //TODO return value from database
+            return false;
+        },
+        options:function () {
+            return JSON.stringify(this.conditions);
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
