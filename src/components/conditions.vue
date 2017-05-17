42b492723c72ce4c4da29ee5af633287564c9458
tawanda <tawanda.nyakudjga@gmail.com>
2017-05-17 16:59:52 +0800

    conditions component

---
+++ src/components/conditions.vue
@@ -0,0 +1,29 @@
+<script>
+    export default {
+        props:["conditions","renderer","name"],
+        mounted(){
+
+        },
+        render(h) {
+            let vm =this;
+            var conditions = vm.conditions;
+            var values = Object.keys(conditions);
+            if (!values) {
+                return (
+                    <div> no conditions </div>
+                )
+            }else{
+                return (
+                    <div id={"cons_"+vm.name} class="hidden">
+                        {values.map(val =>{
+                            {return conditions[val].map(c=>{
+                                {return vm.renderer.renderChildren(h,c)}
+                            })}
+                        })}
+                    </div>
+                )
+            }
+
+        }
+    }
+</script>
