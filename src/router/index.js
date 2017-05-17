b54330a134228f6c24cec92cc2f3bf083d52dfc5
tawanda <tawanda.nyakudjga@gmail.com>
2017-05-17 17:01:45 +0800

    default route to forms

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import Forms from '@/components/Form'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    },
-    {
-      path: '/forms',
-      name: 'forms',
-      component: Forms
-    },
-  ]
+  'mode': 'history',
+  routes: [{
+    path: '/',
+    name: 'forms',
+    component: Forms
+  }, ]
 })
