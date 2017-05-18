88a74f3bebf91c367dccc76347fd862e9dade6d4
tawanda <tawanda.nyakudjga@gmail.com>
2017-05-18 17:10:38 +0800

    error check conditions

--- src/components/conditions.vue
+++ src/components/conditions.vue
@@ -6,24 +6,30 @@
         },
         render(h) {
             let vm =this;
-            var conditions = vm.conditions;
-            var values = Object.keys(conditions);
-            if (!values) {
-                return (
-                    <div> no conditions </div>
-                )
-            }else{
-                return (
-                    <div id={"cons_"+vm.name} class="hidden">
-                        {values.map(val =>{
-                            {return conditions[val].map(c=>{
-                                {return vm.renderer.renderChildren(h,c)}
-                            })}
-                        })}
-                    </div>
-                )
+            try {
+                var conditions = vm.conditions;
+                if (conditions) {
+                    var values = Object.keys(conditions);
+                    if (!values) {
+                        return (
+                            <div> no conditions </div>
+                        )
+                    }else{
+                        return (
+                            <div id={"cons_"+vm.name} class="hidden">
+                                {values.map(val =>{
+                                    {return conditions[val].map(c=>{
+                                        {return vm.renderer.renderChildren(h,c)}
+                                    })}
+                                })}
+                            </div>
+                        )
+                    }
+                }
+            } catch (e) {
+                console.log(e);
             }
-
         }
+
     }
 </script>
