0d453b7e63ebb11ea9755d87ac8b47c6eeda64de
tawanda <tawanda.nyakudjga@gmail.com>
2017-05-22 16:18:05 +0800

    add support for autofilled forms with data

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,7 +5,13 @@
     import Radio from './radio.vue'
     import Renderer from '../utils/renderer'
     const jdata = require('@/assets/data')
+    const jans = require('@/assets/ans')
 
+    let fdata = {};
+    for (var a in jans) {
+        var key = Object.keys(jans[a])[0];
+        fdata[key] = jans[a][key][0];
+    }
     export default {
         methods:{
 
@@ -15,7 +21,7 @@
             return (
                 <div>
                     {jdata.map(d =>{
-                        return Renderer.renderChildren(h,d)
+                        return Renderer.renderChildren(h,d,fdata)
                     })}
                 </div>
             )
